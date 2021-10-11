3f8e6cc5ba579b364b13a4aa88749f066c3de0fd
kidjung <kjpark4321@naver.com>
2021-10-11 14:36:49 +0900

    feat: curprice api 작성

--- main.py
+++ main.py
@@ -1,12 +1,14 @@
 from flask import Flask, current_app, request, jsonify, Response
 import dataframeconvertor
+import currentstockcrawler
+import json
 app = Flask (__name__)
  
 
 
 
 @app.route('/predict/MACD', methods=['GET'])
-def make_MACD():
+def makeMACD():
     # prices=db.throwQuery("""SELECT name, COUNT(*) from price GROUP BY name;""")ß
     # return str(prices)
     stock_name = request.args.get('name')
@@ -18,7 +20,7 @@ def make_MACD():
     
 
 @app.route('/predict/recommendStock', methods=['GET'])
-def make_Recommand():
+def makeRecommand():
     # prices=db.throwQuery("""SELECT name, COUNT(*) from price GROUP BY name;""")
     # return str(prices)
     stock_name = request.args.get('name')
@@ -27,6 +29,21 @@ def make_Recommand():
     resultDf = targetDf.iloc[-1].to_json(orient="index", force_ascii=False, date_format='iso')
     # print(resultDf)
     return Response(resultDf, content_type='application/json; charset=utf-8')
+
+
+
+@app.route('/curprice', methods=['GET'])
+def stockPrice():
+    
+    stockCode = request.args.get('code')
+    time, price = currentstockcrawler.crawlStockData(stockCode)
+    
+    bodyData = {}
+    bodyData['time'] = time
+    bodyData['price'] = price
+    bodyJson = json.dumps(bodyData)
+    
+    return Response(bodyJson, content_type='application/json; charset=utf-8')
         
 if __name__ == "__main__":
     app.run()
